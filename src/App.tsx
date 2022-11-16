78fffe6b7df5ec315c8faef36664733a83989e2b
IJTINK7 <ijtink7@mail.ru>
2022-11-16 13:02:53 +0300

    Refactoring the code via components

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./12-Components/Header";
+import {Navigation} from "./12-Components/Navigation";
+import {Content} from "./12-Components/Content";
 
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="header">
-          <img src="https://i.pinimg.com/originals/0a/25/0c/0a250c8816e7118e07e869ae51c54cc0.jpg" alt="logo-img"/>
-      </header>
+      <Header/>
       <main className="main">
-          <nav>
-              <div className="profile">Profile</div>
-              <div className="messages">Messages</div>
-              <div className="mainContent">Main content</div>
-          </nav>
-          <div className="content">
-              <div className="imgContainer">
-                  <img src="https://phonoteka.org/uploads/posts/2021-06/1624448731_17-phonoteka_org-p-akatsuki-oboi-vertikalnie-krasivo-17.jpg" alt="content-img"/>
-              </div>
-              <div>Ava+Description</div>
-              <div>My posts
-                  <div>New Post</div>
-                  <div>Post 1</div>
-                  <div>Post 2</div>
-              </div>
-          </div>
-
+          <Navigation/>
+          <Content/>
       </main>
     </div>
   );
