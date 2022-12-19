02f6ba74d0c9f9116d1c661b9878abf0bc1e3343
IJTINK7 <ijtink7@mail.ru>
2022-12-19 23:29:56 +0300

    Connect Header.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import "./12-Components/Header.css"
 import {Header} from "./12-Components/Header";
 import {Navigation} from "./12-Components/Navigation";
 import {Content} from "./12-Components/Content";
