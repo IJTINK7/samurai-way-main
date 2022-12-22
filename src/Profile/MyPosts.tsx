4ec242aee6fcc4cf5dfb08be59103703f74f070c
IJTINK7 <ijtink7@mail.ru>
2022-12-22 17:49:35 +0300

    Change styles

--- src/Profile/MyPosts.tsx
+++ src/Profile/MyPosts.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import s from "./Posts.module.css";
 
 export const MyPosts = () => {
 	return (
 		<div>My posts
-			<div>New Post</div>
-			<div>Post 1</div>
-			<div>Post 2</div>
+			<div>
+				<textarea></textarea>
+				<button>Add post</button>
+			</div>
+			<div className={s.posts}>
+				<div>Post 1</div>
+				<div>Post 2</div>
+			</div>
+
 		</div>
 	);
 }
