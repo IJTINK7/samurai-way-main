7f5b64d6fe8a4c6e6f8458e53d2194808961a913
IJTINK7 <ijtink7@mail.ru>
2023-07-30 19:28:41 +0300

    Creating Header.tsx file

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const Header = () => {
+	return (
+		<div>
+			<div>Home</div>
+			<div>News</div>
+			<div>Feed</div>
+			<div>Messages</div>
+		</div>
+	);
+};
