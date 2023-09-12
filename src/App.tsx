3a0a902796cf54b04875ca42cffbd12fec796612
IJTINK7 <ijtink7@mail.ru>
2023-09-12 17:39:50 +0300

    Changing App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
-import {Main} from "./components/Main";
+import {Navigation} from "./components/Navigation";
+import {Content} from "./components/Content";
 import {Footer} from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
         <Header/>
-        <Main/>
-        <Footer/>
+        <div className={"navigation_content_container"}>
+            <Navigation/>
+            <Content/>
+        </div>
+      <Footer/>
     </div>
   );
 }
