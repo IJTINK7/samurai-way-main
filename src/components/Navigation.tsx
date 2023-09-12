a0cae3477b6fef77640098b798b2c5311845660c
IJTINK7 <ijtink7@mail.ru>
2023-09-12 17:39:42 +0300

    Changing Navigation.tsx file

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Navigation = () => {
+	return(
+		<div className={"navigation_wrapper"}>
+			<div>Home</div>
+			<div>Messages</div>
+			<div>News</div>
+			<div>Feed</div>
+		</div>
+	);
+}
