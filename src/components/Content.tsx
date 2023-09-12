aba62102876c8b993d1fda6de99b051deea5bf1b
IJTINK7 <ijtink7@mail.ru>
2023-09-12 17:39:15 +0300

    Creating Content.tsx file

---
+++ src/components/Content.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Content = () => {
+	return (
+		<div className={"content_wrapper"}>
+			Content
+			<div>
+				<input type="text" className={"content_wrapper_input"}/>
+				<button className={"content_wrapper_button"}>Add Post</button>
+			</div>
+		</div>
+	);
+};
