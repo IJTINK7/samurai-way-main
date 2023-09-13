5fb48c4f6ea4d7e1b12e99c3b15e744c33cea2d9
IJTINK7 <ijtink7@mail.ru>
2023-09-13 16:02:24 +0300

    Changing the component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import {Header} from "./components/Header";
 import {Navigation} from "./components/Navigation";
-import {Content} from "./components/Content";
+import {Profile} from "./components/Profile";
 import {Footer} from "./components/Footer";
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
         <Header/>
         <div className={"navigation_content_container"}>
             <Navigation/>
-            <Content/>
+            <Profile/>
         </div>
       <Footer/>
     </div>
