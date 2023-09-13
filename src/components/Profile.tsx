4d55eaddae8eeef45f8b74fdd2f5327b99839d8b
IJTINK7 <ijtink7@mail.ru>
2023-09-13 16:01:51 +0300

    Renaming the component

---
+++ src/components/Profile.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const Profile = () => {
+	return (
+		<div className={"profile_wrapper"}>
+			Content
+			<div>
+				<input type="text" className={"profile_wrapper_input"}/>
+				<button className={"profile_wrapper_button"}>Add Post</button>
+			</div>
+		</div>
+	);
+};
