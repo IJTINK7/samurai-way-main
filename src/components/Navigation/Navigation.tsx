3c575eb2a856af3836024f2f63e4a6c8db212e8b
IJTINK7 <ijtink7@mail.ru>
2023-09-27 11:24:21 +0300

    Adding links to navigation elements

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,10 +3,10 @@ import s from "./Navigation.module.css"
 export const Navigation = () => {
 	return(
 		<div className={s.navigation_wrapper}>
-			<div>Home</div>
-			<div>Messages</div>
-			<div>News</div>
-			<div>Feed</div>
+			<div><a href="/home">Home</a></div>
+			<div><a href="/dialogs">Dialogs</a></div>
+			<div><a href="/news">News</a></div>
+			<div><a href="/feed">Feed</a></div>
 		</div>
 	);
 }
