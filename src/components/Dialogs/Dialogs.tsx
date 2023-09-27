4228ef70a4b18ba34ee858dcca367cfb2c28804a
IJTINK7 <ijtink7@mail.ru>
2023-09-27 18:25:55 +0300

    Changing Dialogs.tsx layout

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,7 +4,16 @@ type PropsType = {}
 export const Dialogs: React.FC<PropsType> = () => {
 	return (
 		<div className={s.dialogs_wrapper}>
-			Dialogs
+			<div className={s.dialogs_items}>
+				<div className={s.dialogs_item}>Minato</div>
+				<div className={s.dialogs_item}>Kakashi</div>
+				<div className={s.dialogs_item}>Rin</div>
+			</div>
+			<div className={s.dialogs_messages}>
+				<div className={s.dialogs_message}>You and I will become Hokage.</div>
+				<div className={s.dialogs_message}>Do you know what happens to people who break the rules?</div>
+				<div className={s.dialogs_message}>Become Hokage and show me how you'll save the world!</div>
+			</div>
 		</div>
 	);
 };
