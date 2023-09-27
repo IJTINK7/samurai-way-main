4db4816c846a4f33940c2ec31f2844ea32cc6185
IJTINK7 <ijtink7@mail.ru>
2023-09-27 16:02:31 +0300

    Changing "a" tag to "NavLink" react-router-dom tag

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import s from "./Navigation.module.css"
+import {NavLink} from "react-router-dom";
 export const Navigation = () => {
 	return(
 		<div className={s.navigation_wrapper}>
-			<div><a href="/home">Home</a></div>
-			<div><a href="/dialogs">Dialogs</a></div>
-			<div><a href="/news">News</a></div>
-			<div><a href="/feed">Feed</a></div>
+			<div><NavLink to="/home">Home</NavLink></div>
+			<div><NavLink to="/dialogs">Dialogs</NavLink></div>
+			<div><NavLink to="/news">News</NavLink></div>
+			<div><NavLink to="/feed">Feed</NavLink></div>
 		</div>
 	);
 }
