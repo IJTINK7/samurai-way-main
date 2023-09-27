6536d4d854defeb6a435c7f157686f69ca5c2097
IJTINK7 <ijtink7@mail.ru>
2023-09-27 16:22:46 +0300

    Changing active links styles

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -4,10 +4,10 @@ import {NavLink} from "react-router-dom";
 export const Navigation = () => {
 	return(
 		<div className={s.navigation_wrapper}>
-			<div><NavLink to="/home">Home</NavLink></div>
-			<div><NavLink to="/dialogs">Dialogs</NavLink></div>
-			<div><NavLink to="/news">News</NavLink></div>
-			<div><NavLink to="/feed">Feed</NavLink></div>
+			<div><NavLink to="/home" className={({isActive})=>isActive ? s.activeLink : s.navLink}>Home</NavLink></div>
+			<div><NavLink to="/dialogs" className={({isActive})=>isActive ? s.activeLink : s.navLink}>Dialogs</NavLink></div>
+			<div><a href="/news">News</a></div>
+			<div><a href="/feed">Feed</a></div>
 		</div>
 	);
 }
