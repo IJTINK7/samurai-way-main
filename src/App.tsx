66733e78058ba6563a72abb876f3fd6edde03f06
IJTINK7 <ijtink7@mail.ru>
2023-09-27 12:24:09 +0300

    Showing content depending on clicked link in Navigation menu

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,29 @@ import {Header} from "./components/Header/Header";
 import {Navigation} from "./components/Navigation/Navigation";
 import {Footer} from "./components/Footer/Footer";
 import {Dialogs} from "./components/Dialogs/Dialogs";
+import {Profile} from "./components/Profile/Profile";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <Header/>
-        <div className={"navigation_profile_container"}>
-            <Navigation/>
-            {/*<Profile/>*/}
-            <Dialogs/>
-        </div>
-      <Footer/>
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <Header/>
+              <div className={"navigation_content_container"}>
+                  <Navigation/>
+                  <div className={"content_container"}>
+                      <Routes>
+                          <Route path="/home" element={<Profile/>}/>
+                          <Route path="/dialogs" element= {<Dialogs/>}/>
+                      </Routes>
+                  </div>
+              </div>
+
+
+
+              <Footer/>
+          </div>
+      </BrowserRouter>
   );
 }
 
