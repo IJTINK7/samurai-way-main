20902213c90af2a191724e8ec35b49892430c4bc
IJTINK7 <ijtink7@mail.ru>
2023-10-05 12:54:09 +0300

    Changing styles for navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,11 +3,11 @@ import s from "./Navigation.module.css"
 import {NavLink} from "react-router-dom";
 export const Navigation = () => {
 	return(
-		<div className={s.navigation_wrapper}>
-			<div><NavLink to="/home" className={({isActive})=>isActive ? s.activeLink : s.navLink}>Home</NavLink></div>
-			<div><NavLink to="/dialogs" className={({isActive})=>isActive ? s.activeLink : s.navLink}>Dialogs</NavLink></div>
-			<div><a href="/news">News</a></div>
-			<div><a href="/feed">Feed</a></div>
-		</div>
-	);
+	<div className={s.navigation_wrapper}>
+		<div><NavLink to={"/home"} className={({isActive}) => (isActive ? s.activeLink : s.inactiveLink)}>Home</NavLink></div>
+		<div><NavLink to={"/dialogs"} className={({isActive}) => (isActive ? s.activeLink : s.inactiveLink)}>Dialogs</NavLink></div>
+		<div><NavLink to={"/news"} className={({isActive}) => (isActive ? s.activeLink : s.inactiveLink)}>News</NavLink></div>
+		<div><NavLink to={"/settings"} className={({isActive}) => (isActive ? s.activeLink : s.inactiveLink)}>Feed</NavLink></div>
+	</div>
+);
 }
