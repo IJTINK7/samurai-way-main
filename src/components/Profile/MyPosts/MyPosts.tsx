d065f61fd24c57e29128541c29d0d67926ff55d7
IJTINK7 <ijtink7@mail.ru>
2023-10-05 16:50:52 +0300

    Changing React.createRef() to useRef() hook

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,8 +1,8 @@
-import React, {ChangeEvent, useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import s from "./MyPosts.module.css"
 import {Post} from "./Post/Post";
 import {PostsInfoType} from "../../../state/state";
-import {v1} from "uuid";
+// import {v1} from "uuid";
 
 
 type MyPostsPropsType={
@@ -14,12 +14,16 @@ export const MyPosts: React.FC<MyPostsPropsType> = ({postsInfo}) => {
 	const [postsValue, setPostsValue] = useState("")
 	// const [postTextValue, setPostTextValue] = useState("Add this string to new post")
 
-	let newPostElement = React.createRef<HTMLInputElement>();
+	// let newPostElement = React.createRef<HTMLInputElement>(); // for class components
+	let newPostElement = useRef(null); // for functional components
 
 	const setPostsValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		setPostsValue(e.currentTarget.value)
 	}
 	const sendPost = () => {
+		if (newPostElement.current !== null) {
+			console.log((typeof newPostElement.current))
+		}
 		// let newPost = {id: v1(), postText: "NEW POST TEXT", likesCount: 0}
 		// setPostsValueHandler(newPost)
 		// setPostsValue(newPostElement.current.value)
@@ -29,7 +33,7 @@ export const MyPosts: React.FC<MyPostsPropsType> = ({postsInfo}) => {
 		<div className={s.my_posts_wrapper}>
 				<div>My posts</div>
 				<div>
-					<input ref={newPostElement} type="text" value={postsValue} className={s.my_posts_wrapper_input} onChange={setPostsValueHandler}/>
+					<input type="text" value={postsValue} className={s.my_posts_wrapper_input} onChange={setPostsValueHandler}/>
 					<button className={s.my_posts_wrapper_button} onClick={sendPost}>Add Post</button>
 				</div>
 
