44071fa31cb688ebfb9750a209cc2d3ca2378d9c
IJTINK7 <ijtink7@mail.ru>
2023-10-05 16:43:03 +0300

    Changing "MyPosts" component with adding React.createRef() and adding some logic to "senPost" function

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,7 @@ import React, {ChangeEvent, useState} from 'react';
 import s from "./MyPosts.module.css"
 import {Post} from "./Post/Post";
 import {PostsInfoType} from "../../../state/state";
+import {v1} from "uuid";
 
 
 type MyPostsPropsType={
@@ -9,20 +10,26 @@ type MyPostsPropsType={
 }
 
 export const MyPosts: React.FC<MyPostsPropsType> = ({postsInfo}) => {
+
 	const [postsValue, setPostsValue] = useState("")
 	// const [postTextValue, setPostTextValue] = useState("Add this string to new post")
+
+	let newPostElement = React.createRef<HTMLInputElement>();
+
 	const setPostsValueHandler = (e: ChangeEvent<HTMLInputElement>) => {
 		setPostsValue(e.currentTarget.value)
 	}
 	const sendPost = () => {
-		// setPostTextValue(postsValue)
-		setPostsValue("")
+		// let newPost = {id: v1(), postText: "NEW POST TEXT", likesCount: 0}
+		// setPostsValueHandler(newPost)
+		// setPostsValue(newPostElement.current.value)
 	}
+
 	return (
 		<div className={s.my_posts_wrapper}>
 				<div>My posts</div>
 				<div>
-					<input type="text" value={postsValue} className={s.my_posts_wrapper_input} onChange={setPostsValueHandler}/>
+					<input ref={newPostElement} type="text" value={postsValue} className={s.my_posts_wrapper_input} onChange={setPostsValueHandler}/>
 					<button className={s.my_posts_wrapper_button} onClick={sendPost}>Add Post</button>
 				</div>
 
