b5da83baff10a2054d9901c5a7cba9aa131b48eb
IJTINK7 <ijtink7@mail.ru>
2023-11-22 23:58:08 +0300

    Changing Profile.tsx via "dispatch" method

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 import s from "./Profile.module.css"
 import {ProfileHeaderInfo} from "./ProfileHeaderInfo/ProfileHeaderInfo";
 import {MyPosts} from "./MyPosts/MyPosts";
-import {ProfilePageType} from "../../state/state";
+import {ActionType, ProfilePageType} from "../../state/state";
 
 
 type ProfilePropsType = {
 	profilePage: ProfilePageType
-	addPost: () => void
-	newPostText: string
-	updateNewPostText: (newText: string) => void
+	dispatch:(action: ActionType)=> void
 }
 
-export const Profile: React.FC<ProfilePropsType> = ({profilePage, addPost, newPostText, updateNewPostText}) => {
+export const Profile: React.FC<ProfilePropsType> = ({profilePage, dispatch}) => {
 	return (
 		<div className={s.profile_wrapper}>
 			<div>
 				<ProfileHeaderInfo/>
 			</div>
 			<div>
-				<MyPosts posts={profilePage.posts} addPost={addPost} newPostText={newPostText}
-						 updateNewPostText={updateNewPostText}/>
+				<MyPosts posts={profilePage.posts} dispatch={dispatch}/>
 			</div>
 		</div>
 	);
