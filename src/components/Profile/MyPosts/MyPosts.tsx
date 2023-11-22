c50ccc0b2a513dd4a6040535e0c209be0bbb9f6a
IJTINK7 <ijtink7@mail.ru>
2023-11-22 23:58:49 +0300

    Changing MyPosts.tsx via "dispatch" method

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,30 +1,30 @@
 import React, {ChangeEvent} from 'react';
 import s from "./MyPosts.module.css"
 import {Post} from "./Post/Post";
-import {PostsInfoType} from "../../../state/state";
+import {ActionType, PostsInfoType} from "../../../state/state";
 
 type MyPostsPropsType = {
 	posts: PostsInfoType
-	addPost: () => void
-	newPostText: string
-	updateNewPostText: (newText: string) => void
+	dispatch: (action: ActionType) => void
 }
 
-export const MyPosts: React.FC<MyPostsPropsType> = ({posts, newPostText, addPost, updateNewPostText}) => {
+export const MyPosts: React.FC<MyPostsPropsType> = ({posts,dispatch}) => {
 
 	const onPostChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-		updateNewPostText(e.currentTarget.value)
+		let newText = e.currentTarget.value
+		dispatch({type: "UPDATE-NEW-POST-TEXT", payload: {newText}})
 	}
 	const addPostHandler = () => {
-		addPost()
-		updateNewPostText("")
+		let newText = ""
+		dispatch({type: "ADD-POST", payload:{newText}})
+		dispatch({type: "UPDATE-NEW-POST-TEXT", payload: {newText}})
 	}
 
 	return (
 		<div className={s.my_posts_wrapper}>
 			<div>My posts</div>
 			<div>
-				<input value={newPostText} className={s.my_posts_wrapper_input} onChange={onPostChangeHandler}/>
+				<input value={"newPostText"} className={s.my_posts_wrapper_input} onChange={onPostChangeHandler}/>
 				<button className={s.my_posts_wrapper_button} onClick={addPostHandler}>Add Post</button>
 			</div>
 
