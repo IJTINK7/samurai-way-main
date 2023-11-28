3d617f2185593ed9e0476766acdbe45a83c6d3d5
IJTINK7 <ijtink7@mail.ru>
2023-11-28 23:40:24 +0300

    Creating "onNewMessageChange" and "onSendMessageClick" for completing of adding new messages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from "./Dialogs.module.css"
 import {DialogsItem} from "./DialogsItem";
 import {DialogsMessage} from "./DialogsMessage";
-import {DialogsPageType} from "../../state/state";
+import {DialogsPageType, sendMessageAC, store, updateNewMessageBodyAC} from "../../state/state";
 
 export const Dialogs: React.FC<DialogsPageType> = ({dialogsItems, dialogsMessages, newMessageBody}) => {
+	const onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+		let body = e.currentTarget.value
+		store.dispatch(updateNewMessageBodyAC(body))
+	}
+	const onSendMessageClick = () => {
+		store.dispatch(sendMessageAC())
+	}
 	return (
 		<div className={s.dialogs_wrapper}>
 			<div className={s.dialogs_items}>
 				{dialogsItems.map(el => <DialogsItem id={el.id} title={el.name}/>)}
 			</div>
 			<div className={s.dialogs_messages}>
-				{dialogsMessages.map(el => <DialogsMessage title={el.title}/>)}
+				<div>{dialogsMessages.map(el => <DialogsMessage title={el.title}/>)}</div>
+				<div>
+					<div><textarea value={newMessageBody} onChange={onNewMessageChange} placeholder={"Enter your message"}></textarea></div>
+					<div><button onClick={onSendMessageClick}>Send</button></div>
+				</div>
 			</div>
 		</div>
 	);
