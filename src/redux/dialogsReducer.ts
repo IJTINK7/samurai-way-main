63f3832c256df27ec5ba1e5a06cc777ff307569a
IJTINK7 <ijtink7@mail.ru>
2023-11-29 14:33:53 +0300

    Changing dialogsReducer.ts file

--- src/redux/dialogsReducer.ts
+++ src/redux/dialogsReducer.ts
@@ -4,9 +4,9 @@ export const dialogsReducer = (state: any, action: any) => {
 			state.newMessageBody = action.payload.body
 			return state;
 		case "SEND-MESSAGE":
-			let body = state.dialogsPage.newMessageBody;
-			state.dialogsPage.newMessageBody = ""
-			state.dialogsPage.dialogsMessages.push({id: 4, title: body})
+			let body = state.newMessageBody;
+			state.newMessageBody = ""
+			state.dialogsMessages.push({id: 4, title: body})
 			return state;
 		default:
 			return state;
