8a0f3440169e611d320e2e4770252ac003a520d2
IJTINK7 <ijtink7@mail.ru>
2023-11-29 14:09:39 +0300

    Creating profileReducer.ts file

---
+++ src/redux/profileReducer.ts
@@ -0,0 +1,15 @@
+import {PostsInfoType} from "./state";
+
+export const profileReducer = (state: any, action: any) => {
+	switch (action.type){
+		case "ADD-POST" :
+			let newPost: PostsInfoType = {id: 4, postText: state.newPostText, likesCount: 0};
+			state.posts.push(newPost);
+			state.newPostText = "";
+			return state;
+		case "UPDATE-NEW-POST-TEXT":
+			state.newPostText = action.payload.newText;
+			return state;
+		default: return state;
+	}
+}
