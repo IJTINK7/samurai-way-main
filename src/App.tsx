2b9546da0b16837f4cad4d9a0d14e0cc8af1b61b
IJTINK7 <ijtink7@mail.ru>
2024-02-04 18:05:11 +0300

    Adding "DialogsContainer" contained component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,11 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navigation} from "./components/Navigation/Navigation";
 import {Footer} from "./components/Footer/Footer";
-import {Dialogs} from "./components/Dialogs/Dialogs";
 import {Profile} from "./components/Profile/Profile";
 import {Route, Routes} from "react-router-dom";
 import {News} from "./components/News/News";
 import {Settings} from "./components/Settings/Settings";
+import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 
 const App = () => {
 	return (
@@ -20,7 +20,7 @@ const App = () => {
 						<Route path="/" element={<Profile/>}/>
 						<Route path="/home/*" element={<Profile/>}/>
 						<Route path="/dialogs/*"
-							   element={<Dialogs/>}/>
+							   element={<DialogsContainer/>}/>
 						<Route path="/news/*" element={<News/>}/>
 						<Route path="/settings/*" element={<Settings/>}/>
 					</Routes>
---
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Dialogs} from "./Dialogs";
+
+export const DialogsContainer = () => {
+	return (
+		<Dialogs/>
+	);
+};
